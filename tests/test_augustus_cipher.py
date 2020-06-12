e26d4c94a203e0af9c771d35017a848b3d957151
PureFunctor <purefunctor@gmail.com>
2020-06-12 12:03:35 +0800

    Update cipher tests to include tabs and newlines

--- tests/test_augustus_cipher.py
+++ tests/test_augustus_cipher.py
@@ -2,10 +2,20 @@ from augustus.cipher import SteppedAugustus
 
 
 def test_SteppedAugustus_right_cipher():
-    s = SteppedAugustus("Hello World")
-    assert s.right_cipher == "Igopt Xqupi"
+    s0 = SteppedAugustus("Hello World")
+    s1 = SteppedAugustus("Hello\tWorld")
+    s2 = SteppedAugustus("Hello\nWorld")
+
+    assert s0.right_cipher == "Igopt Xqupi"
+    assert s1.right_cipher == "Igopt\tXqupi"
+    assert s2.right_cipher == "Igopt\nXqupi"
 
 
 def test_SteppedAugustus_left_cipher():
-    s = SteppedAugustus("Igopt Xqupi")
-    assert s.left_cipher == "Hello World"
+    s0 = SteppedAugustus("Igopt Xqupi")
+    s1 = SteppedAugustus("Igopt\tXqupi")
+    s2 = SteppedAugustus("Igopt\nXqupi")
+
+    assert s0.left_cipher == "Hello World"
+    assert s1.left_cipher == "Hello\tWorld"
+    assert s2.left_cipher == "Hello\nWorld"
